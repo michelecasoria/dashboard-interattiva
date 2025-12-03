af55347d733a05e0c8b7fe1190daf1ff98648594
Michele Casoria <mcasoria@gmail.com>
2025-12-03 16:31:25 +0100

    Aggiornato viewer.js per dropdown dinamico dai PDB

--- js/viewer.js
+++ js/viewer.js
@@ -5,21 +5,38 @@ const viewer = new Molstar.Viewer("viewer", {
 
 // Funzione per caricare un PDB
 function loadPDB(pdbPath) {
+  viewer.clear(); // pulisce prima di caricare
   viewer.loadStructureFromUrl(
     pdbPath,
     "pdb",
     { representationParams: { theme: { customColor: "#ff9999" } } }
   ).then(() => {
-    viewer.clear();
     viewer.setRepresentation("cartoon");
   });
 }
 
-// Dropdown
-const select = document.getElementById("pdb-select");
-select.addEventListener("change", () => {
-  loadPDB(select.value);
-});
+// Carica la lista dei PDB dal JSON e popola il dropdown
+fetch('pdbs/list.json')
+  .then(response => response.json())
+  .then(pdbList => {
+    const select = document.getElementById("pdb-select");
 
-// Carica il primo PDB di default
-loadPDB(select.value);
+    // Popola dropdown
+    pdbList.forEach(pdb => {
+      const option = document.createElement('option');
+      option.value = "pdbs/" + pdb;  // percorso corretto
+      option.text = pdb;
+      select.appendChild(option);
+    });
+
+    // Evento cambio selezione
+    select.addEventListener("change", () => {
+      loadPDB(select.value);
+    });
+
+    // Carica il primo PDB di default
+    if (pdbList.length > 0) {
+      select.value = "pdbs/" + pdbList[0];
+      loadPDB(select.value);
+    }
+  });
